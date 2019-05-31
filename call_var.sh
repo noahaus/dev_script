34165eea881f714fe01aca4b04628f9501751951
Noah Legall <noahaus@uga.edu>
2019-05-31 16:57:12 -0400

    full variant calling command addition

--- call_var.sh
+++ call_var.sh
@@ -2,14 +2,14 @@
 #PBS -q batch
 #PBS -N call_variants
 #PBS -l nodes=1:ppn=4:AMD
-#PBS -l walltime=24:00:00
+#PBS -l walltime=72:00:00
 #PBS -l mem=4gb
 #PBS -M noahaus@uga.edu
 #PBS -m abe
 
 
 cd $PBS_O_WORKDIR
-module add freebayes/1.2.0
+module add BCFtools/1.9-foss-2016b
 
 echo
 echo "Job ID: $PBS_JOBID"
@@ -19,4 +19,13 @@ echo "Nodes:  $(cat $PBS_NODEFILE | sort -u | tr '\n' ' ')"
 echo "mpirun: $(which mpirun)"
 echo
 
-python variant_call.py
+ls | grep "nodup.sorted.bam" > bam_list.txt
+
+
+bcftools mpileup -Ou -f NC_002945v4.fasta -b bam_list.txt > temp.raw.vcf
+bcftools call -Ou --ploidy 1 -mv temp.raw.vcf > temp.call.vcf
+bcftools filter -s LowQual -e '%QUAL<20 || TYPE="indel"' temp.call.vcf > 53_isolates.filter.vcf
+python vcf2phylip.py -i 53_isolates.filter.vcf
+
+mkdir full_vcf_output
+mv 53_isolates.filter* -t full_vcf_output
